8a256dfaa9f5e5e429a9371afcab340827b3a8eb
Mikael Carlsson <mickec79@gmail.com>
2015-08-18 01:01:28 +0200

    Added momentjs and enabled getting local time of map marker.

--- js/stores/create/map.js
+++ js/stores/create/map.js
@@ -1,10 +1,11 @@
 var riot = require('riot');
 var agent = require('superagent');
+var moment = require('moment');
 
 module.exports = function() {
 	riot.observable(this);
 
-	var self = this, map, marker;
+	var self = this, map, marker, coords;
 	var geocoder = new google.maps.Geocoder();
 	var annsjon = new google.maps.LatLng(63.27114510000001, 12.449269500000014);
 	var mapOptions = {
@@ -20,20 +21,14 @@ module.exports = function() {
 		map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
 
 		google.maps.event.addListener(map, 'click', function(e){
-			// Local time
-			var ts = new Date().getTime() / 1000;
-			var url = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
-			agent.get(url + e.latLng.lat() + ',' + e.latLng.lng() + '&timestamp=' + ts).end(function(err, res) {
-				var dstOffset = res.body.dstOffset;
-				var rawOffset = res.body.rawOffset;
-				var localTime = ((rawOffset - dstOffset) + ts) * 1000;
-				console.log(res.body);
-				console.log('local time', new Date(localTime));
-			})
+			coords = e.latLng.lat() + ',' + e.latLng.lng();
 
 			// Marker
 			addMarker(e.latLng);
-			setPositionData(e.latLng);
+			// Location
+			getPositionData(e.latLng);
+			// Time
+			getLocalTime();
 		});
 	}
 
@@ -51,17 +46,31 @@ module.exports = function() {
 		marker.setMap(null);
 	}
 
-	function setPositionData(location) {
+	function getPositionData(location) {
 		var country, data = {};
 		geocoder.geocode({'latLng': location}, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
 				if (results[0]) {
 					country = results[results.length - 1].address_components[0];
-					data.latLng = location.lat() +', '+ location.lng();
+					data.latLng = coords;
 					data.countryIso = country.short_name;
 					data.countryName = country.long_name;
 					self.trigger('map_data', data);
 				}
+			}
+		});
+	}
+
+	function getLocalTime() {
+		var localtime;
+		var timestamp = moment().unix();
+		var baseURL = 'https://maps.googleapis.com/maps/api/timezone/json?location=';
+		var url = baseURL + coords + '&timestamp=' + timestamp + '&key=AIzaSyDU-QSezsFkEm9DikRmj7CH6RQnQIJhq4c';
+		agent.get(url).end(function(err, res) {
+			if (!err && res.status == 200) {
+				var offsets = res.body.dstOffset + res.body.rawOffset;
+				localtime = moment.unix(timestamp + offsets).utc().format('HH:mm:ss, YYYY-MM-DD');
+				console.log('local time:', localtime);
 			}
 		});
 	}
