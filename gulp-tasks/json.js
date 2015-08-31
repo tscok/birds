c0b0ff2a728b7d9d50ada8cafb306b1c74482ea9
Mikael Carlsson <mickec79@gmail.com>
2015-08-31 23:27:01 +0200

    Adjusting JSON task output.

--- gulp-tasks/json.js
+++ gulp-tasks/json.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 
 var csvtojson = require('gulp-csvtojson');
-var convert = require('gulp-convert');
-
-var minifyJSON = require('gulp-jsonminify');
+var rename = require('gulp-rename');
+// var minifyJSON = require('gulp-jsonminify');
 
 // Output directory.
 var __dirname = './dist/';
@@ -11,5 +10,10 @@ var __dirname = './dist/';
 gulp.task('json', function() {
     return gulp.src('docs/ArtlistaFagel3.csv')
         .pipe(csvtojson())
+        // .pipe(minifyJSON())
+        .pipe(rename({
+            basename: 'artlista',
+            suffix: '.min'
+        }))
         .pipe(gulp.dest(__dirname));
 });
